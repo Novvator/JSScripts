7c40bb1ed37fcf49e77f97d1e3253deefe6a32bf
Novvator <stamatis.sellas@yahoo.com>
2022-04-19 13:56:55 +0300

    changes in dinogame added meteor gameplay balance

--- dinogame/script.js
+++ dinogame/script.js
@@ -1,61 +1,111 @@
-var character = document.getElementById("character");
-document.addEventListener("keydown", (e) =>{
-  console.log(e.code)
-  if(e.code == 'ArrowUp' && !isJumping)
-    jump();
-})
+//get elements from document
 
+var character = document.querySelector("#character");
+var replayPrompt = document.querySelector('#replay');
+var block = document.querySelector('#block');
+var meteor = document.querySelector('#meteor');
 var isJumping = false;
 
+//start game
 
-const replay = document.querySelector('#replay');
+function startGame() {
+  document.addEventListener("keydown", (e) =>{
+    if(e.code == 'ArrowUp' && !isJumping)
+      jump();
+  })
 
+  
+  block.classList.add("animate-block");
+  meteor.classList.add("animate-meteor");
+
+  
+}
+
+//character jump function
 function jump() {
   isJumping = true;
-  if(character.classList != "animate") {
-    character.classList.add("animate");
+  if(character.classList != "animate-jump") {
+    character.classList.add("animate-jump");
   }
   setTimeout(function() {
-    if(character.classList == "animate") {
-      character.classList.remove("animate");
+    if(character.classList == "animate-jump") {
+      character.classList.remove("animate-jump");
     }
     isJumping = false;
 
   },1050)
 }
 
-function toggleVisibility(element) {
-  if(window.getComputedStyle(element).visibility === 'hidden') {
-    element.style.visibility = 'visible';
-    console.log('revealed it');
-    document.addEventListener("keydown", (e) =>{
-      if(e.code == 'Space')
-        location.reload();
-    })
-  } else {
-      element.style.visibility = 'hidden';
-      document.removeEventListener("keydown", (e) =>{
-        if(e.code == 'Space')
-          location.reload();
-      })
-  }
-  
-}
+//check if you lost the game
 var checkDead = setInterval(() => {
-  var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+  var characterBottom = parseInt(window.getComputedStyle(character).getPropertyValue("bottom"));
   var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-  if(blockLeft<80 && blockLeft>0 && characterTop>=280){
-    block.style.animation = "none";
-    block.style.display = "none";
-    alert("u lose");
-    toggleVisibility(replay);
-
+  var meteorLeft = parseInt(window.getComputedStyle(meteor).getPropertyValue("left"));
+  if(blockLeft<100 && blockLeft>-40 && characterBottom<=65){
+    endGame();
+  }
+  if(meteorLeft<120 && meteorLeft>0 && characterBottom>=40) {
+    endGame();
   }
 }, 10);
 
+
+//stop animations when you lose
+function endGame() {
+  document.removeEventListener("keydown", (e) =>{
+    if(e.code == 'ArrowUp' && !isJumping)
+      jump();
+  })
+  block.classList.remove("animate-block");
+  block.style.display = "none";
+  meteor.classList.add("animate-meteor");
+  meteor.style.display = "none";
+  alert("u lose");
+
+  if(window.getComputedStyle(replayPrompt).visibility === 'hidden') {
+    replayPrompt.style.visibility = 'visible';
+    document.addEventListener("keydown", (e) =>{
+      if(e.code == 'Space')
+        resetGame();
+    })
+  }
+}
+
+//reset game after losing
+function resetGame() {
+  if(window.getComputedStyle(replayPrompt).visibility === 'visible') {
+    replayPrompt.style.visibility = 'hidden';
+      document.removeEventListener("keydown", (e) =>{
+        if(e.code == 'Space')
+          resetGame();
+      })
+  }
+  document.addEventListener("keydown", (e) =>{
+    if(e.code == 'ArrowUp' && !isJumping)
+      jump();
+  })
+  block.classList.add("animate-block");
+  block.style.display = "block";
+  meteor.classList.add("animate-meteor");
+  meteor.style.display = "block";
+}
+
+
+startGame();
+
+
+
+
+
+
+
+  
+
+
+
 //use async .then ☑
-//dont use reload to restart
+//dont use reload to restart ☑
 //stop dino motion when lose
-//add meteorite
-//randomize interval of coming projectiles
-//make randomized interval fair
+//add meteorite ☑
+//randomize interval of coming projectiles ☑
+//balance randomized interval
